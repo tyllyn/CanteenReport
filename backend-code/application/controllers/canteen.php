fffe9c13e3d9ce34aa08f32d266e266fce9d6d3e
tyllyn <joshie@gmail.com>
2014-02-22 15:18:47 -0500

    I did something

--- backend-code/application/controllers/canteen.php
+++ backend-code/application/controllers/canteen.php
@@ -7,6 +7,97 @@ class Canteen extends CI_Controller {
 	}
 	
 	public function add($json) {
+	$json='[{
+    "name": "incident-start",
+    "value": ""
+}, {
+    "name": "incident-location",
+    "value": ""
+}, {
+    "name": "incident-route",
+    "value": ""
+}, {
+    "name": "team-driver",
+    "value": ""
+}, {
+    "name": "team-member-1",
+    "value": ""
+}, {
+    "name": "team-member-2",
+    "value": ""
+}, {
+    "name": "team-member-3",
+    "value": ""
+}, {
+    "name": "team-refferal-name",
+    "value": ""
+}, {
+    "name": "team-refferal-title",
+    "value": ""
+}, {
+    "name": "team-drink-coffee-amount",
+    "value": ""
+}, {
+    "name": "team-drink-cocoa-amount",
+    "value": ""
+}, {
+    "name": "team-drink-tea-amount",
+    "value": ""
+}, {
+    "name": "team-cold-drinks-amount",
+    "value": ""
+}, {
+    "name": "team-water-amount",
+    "value": ""
+}, {
+    "name": "team-food-donuts-amount",
+    "value": ""
+}, {
+    "name": "team-food-cookies-amount",
+    "value": ""
+}, {
+    "name": "team-food-sandwiches-amount",
+    "value": ""
+}, {
+    "name": "team-food-hot-dogs-amount",
+    "value": ""
+}, {
+    "name": "team-water-amount",
+    "value": ""
+}, {
+    "name": "team-clothing-gloves-amount",
+    "value": ""
+}, {
+    "name": "team-clothing-blankets-amount",
+    "value": ""
+}, {
+    "name": "team-clothing-amount",
+    "value": ""
+}, {
+    "name": "team-clothing-hand-warmers--amount",
+    "value": ""
+}, {
+    "name": "services-counseling-administer",
+    "value": ""
+}, {
+    "name": "services-counseling-individual",
+    "value": ""
+}, {
+    "name": "services-counseling-reason",
+    "value": ""
+}, {
+    "name": "services-counseling-phone-number",
+    "value": ""
+}, {
+    "name": "end-time",
+    "value": ""
+}, {
+    "name": "end-total-mileage",
+    "value": ""
+}, {
+    "name": "end-route-time",
+    "value": ""
+}]"';
 		$res = json_decode($json);
 		print_r($res);
 	}
