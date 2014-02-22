e3b3efeb04babc94c0d13e27ca843ad086bca4f0
tyllyn <joshie@gmail.com>
2014-02-22 17:54:30 -0500

    a

--- backend-code/application/views/canteen/index.php
+++ backend-code/application/views/canteen/index.php
@@ -8,6 +8,7 @@
 		<li><a href="/backend-code/index.php/canteen/viewitems">View Items</a></li>
 		<li><a href="/backend-code/index.php/canteen/aggitems">All Items Aggregate</a></li>
 		<li><a href="/backend-code/index.php/canteen/aggitems/2014-02-01/2014-03-01/">Items Aggregate - Feb 2014</a></li>
+		<li><a href="/backend-code/index.php/canteen/viewitem/1">Example Item Overview</a></li>
 	</ul>
 	
 	
