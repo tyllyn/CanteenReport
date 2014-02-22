63fce92a48a519c963d7a85d6c72c858f42cb988
Ian Moffitt <nessthehero@gmail.com>
2014-02-22 13:31:20 -0500

    Prototyping offline storage

--- prototype/offline/app/js/main.js
+++ prototype/offline/app/js/main.js
@@ -1,4 +1,5 @@
 /* global window */
+/* global amplify */
 /* global navigator */
 (function ($, window, document, undefined) {
 	'use strict';
@@ -20,6 +21,17 @@
 		} else {
 			PROTO.goOffline();
 		}
+
+		$('#form').on('submit', function (e) {
+			e.preventDefault();
+			console.log(JSON.stringify($('#form').serializeArray()));
+		});
+
+		$('#form').on('blur keydown', 'input, textarea, select', function () {
+			PROTO.saveForm();
+		});
+
+		PROTO.syncForm();
 
 	};
 
@@ -43,27 +55,108 @@
 
 	};
 
+	PROTO.syncForm = function () {
+
+		var unique = $('#form').attr('data-unique');
+
+		console.log(amplify.store(unique));
+
+		if (amplify.store(unique) !== null) {
+
+			PROTO.data.report[unique] = amplify.store(unique);
+
+			for (var key in PROTO.data.report[unique]) {
+				if (PROTO.data.report[unique].hasOwnProperty(key)) {
+					console.log(PROTO.data.report[unique][key], PROTO.data.report[unique][key].name, PROTO.data.report[unique][key].value);
+
+					PROTO.field(PROTO.data.report[unique][key].name, PROTO.data.report[unique][key].value);
+				}
+			}
+
+		}
+
+	};
+
+	PROTO.saveForm = function () {
+
+		var unique = $('#form').attr('data-unique'),
+			formdata = PROTO.getFormJSON();
+
+		//amplify.store(unique, null);
+
+		amplify.store(unique, formdata);
+
+		console.log(amplify.store(unique));
+
+	};
+
+	PROTO.field = function (id, value) {
+		var $field = $('#' + id);
+
+		// console.log('trying', $field, $field.is('input[type=text]'));
+
+		// Text field
+		if ($field.is('input[type=text]')) {
+
+			// console.log('inside');
+
+			console.log($field.val(), value);
+			if ($field.val() !== value) {
+				$field.val(value);
+			}
+
+		}
+
+		if ($field.is('input[type=checkbox]')) {
+
+			// console.log('inside');
+
+			console.log($field.val(), value);
+			if ($field.attr('checked') !== 'checked') {
+				$field.attr('checked', 'checked');
+			}
+
+		}
+
+		if ($field.is('select')) {
+
+			// console.log('inside');
+
+			// console.log($field.find('option[value=' + value + ']'), value);
+			if ($field.val() !== value) {
+				$field.val(value);
+			}
+
+		}
+	};
+
 	PROTO.isOnline = function () {
 		return navigator.onLine;
+	};
+
+	PROTO.getFormJSON = function () {
+		return JSON.parse(JSON.stringify(PROTO.$elm.form.serializeArray()));
 	};
 
 	$(function () {
 
 		// Cached selectors
 		PROTO.$elm = {};
+		PROTO.$elm.form = $('#form');
 
 		// Arbitrary data (Comes after selectors so we can use them here)
 		PROTO.data = {};
 		PROTO.data.online = function () {
 			return PROTO.isOnline();
 		};
+		PROTO.data.report = [];
 
 		PROTO.init();
 
 	});
 
 /** uncomment to see Namespace in the global scope for debugging. */
-	window.Namespace = {
+	window.REI = {
 		PROTO : PROTO
 	};
 /* */
