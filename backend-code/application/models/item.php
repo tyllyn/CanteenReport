4e3fc53ceff480fd6cdb04771e057743a0a6bdff
tyllyn <joshie@gmail.com>
2014-02-22 17:27:00 -0500

    asd

--- backend-code/application/models/item.php
+++ backend-code/application/models/item.php
@@ -48,7 +48,7 @@ class Item extends CI_Model {
 			if ($res == null) {
 				$res = 0;
 			} else {
-				$res = $res[0][0];
+				$res = $res[0];
 			}
 			$data[$start->format('F') . ' ' . $y] = $res;
 			$m = $m - 1;
