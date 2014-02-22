a09177c89503faa0ddfe19b5e6ab9b2e270c4c52
tyllyn <joshie@gmail.com>
2014-02-22 11:30:44 -0500

    testing

--- backend-code/application/controllers/canteen.php
+++ backend-code/application/controllers/canteen.php
@@ -1,26 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Canteen extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+	public function index() {
 		$this->load->view('welcome_message');
 	}
+	
+	public function add(json) {
+		var $res = json_decode(json);
+		print_r($res);
+	}
+	
 }
 
 /* End of file welcome.php */
