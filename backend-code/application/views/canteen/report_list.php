0ebe1aa40be14f9ad06d65818e0d01e338c6b354
tyllyn <joshie@gmail.com>
2014-02-22 21:08:00 -0500

    a

--- backend-code/application/views/canteen/report_list.php
+++ backend-code/application/views/canteen/report_list.php
@@ -27,9 +27,9 @@ foreach ($query as $key => $value) {
 	print "<tr>";
 	
 	$id = 0;
-	print "<td>".$value["incident_unit_number"]."</td>";
-	print "<td>".$value["incident_location"]."</td>";
-	print "<td>".$value["incident_start"]."</td>";
+	print "<td>".$value->incident_unit_number."</td>";
+	print "<td>".$value->incident_location."</td>";
+	print "<td>".$value->incident_start."</td>";
 	
 	// foreach ($value as $col => $val) {
 	
