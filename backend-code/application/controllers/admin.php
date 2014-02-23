5e9d4652e61f04b3b927de03114b6ca6f556bf2f
Eric Miller <ericmller29@gmail.com>
2014-02-22 20:26:17 -0500

    Added records search to report.php

--- backend-code/application/controllers/admin.php
+++ backend-code/application/controllers/admin.php
@@ -8,4 +8,18 @@ class Admin extends REST_Controller{
 		$data = $this->Report->get_entries();
 		$this->response($data);
 	}
+
+	function report_get(){
+		$this->load->model('Report');
+		$params = array(
+			'id' => (isset($_GET['entry_id'])) ? $_GET['entry_id'] : null,
+			'startdate' => (isset($_GET['date'])) ? $_GET['date'] : null,
+			'month' => (isset($_GET['month'])) ? $_GET['month'] : null,
+			'year' => (isset($_GET['year'])) ? $_GET['year'] : null
+		);
+
+
+		$data = $this->Report->get_entry_items($params);
+		$this->response($data);
+	}
 }
