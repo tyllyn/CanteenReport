cd04aa9581853f698a9e368329525ac0050e5126
root <root@li681-146.members.linode.com>
2015-01-15 04:02:02 +0000

    Added https://github.com/eightdotthree/CanteenReportAdmin.git as a submodule, and created a symbolic link

--- backend-code/application/config/database.php
+++ backend-code/application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'codefest';
+$db['default']['password'] = 'kitten14';
+$db['default']['database'] = 'canteen';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- gitupdate.php
+++ gitupdate.php
@@ -1,6 +1,6 @@
 <?php 
 
-exec('git pull origin master');
+exec('echo kitten | sudo -S git pull origin master');
 echo "Updated from Git repo!";
 
 ?>
