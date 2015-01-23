8bcfb1ed2426b34cd5191ba6a7c0b20d125b0114
tyllyn <joshie@gmail.com>
2015-01-23 00:24:20 -0500

    Link Updates

--- backend-code/application/views/canteen/index.php
+++ backend-code/application/views/canteen/index.php
@@ -3,12 +3,12 @@
 	<h1>Test Links</h1>
 
 	<ul>
-		<li><a href="/backend-code/index.php/canteen/viewreports">View Reports</a></li>
-		<li><a href="/backend-code/index.php/canteen/viewreport/1">View A Report</a></li>
-		<li><a href="/backend-code/index.php/canteen/viewitems">View Items</a></li>
-		<li><a href="/backend-code/index.php/canteen/aggitems">All Items Aggregate</a></li>
-		<li><a href="/backend-code/index.php/canteen/aggitems/2014-02-01/2014-03-01/">Items Aggregate - Feb 2014</a></li>
-		<li><a href="/backend-code/index.php/canteen/viewitem/1">Example Item Overview</a></li>
+		<li><a href="/index.php/canteen/viewreports">View Reports</a></li>
+		<li><a href="/index.php/canteen/viewreport/1">View A Report</a></li>
+		<li><a href="/index.php/canteen/viewitems">View Items</a></li>
+		<li><a href="/index.php/canteen/aggitems">All Items Aggregate</a></li>
+		<li><a href="/index.php/canteen/aggitems/2014-02-01/2014-03-01/">Items Aggregate - Feb 2014</a></li>
+		<li><a href="/index.php/canteen/viewitem/1">Example Item Overview</a></li>
 	</ul>
 	
 <?php $this->load->view('footer'); ?>
