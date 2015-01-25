e2310c190dd8eede5d12e4b5940166164fc3b2fd
Joshua Petry <joshie@gmail.com>
2015-01-25 13:45:49 -0500

    Updated rewrites
    Switched to query-strings and added a rewrite that will handle C/M urls.

--- backend-code/application/config/config.php
+++ backend-code/application/config/config.php
@@ -155,7 +155,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 |
 */
 $config['allow_get_array']		= TRUE;
-$config['enable_query_strings'] = FALSE;
+$config['enable_query_strings'] = TRUE;
 $config['controller_trigger']	= 'c';
 $config['function_trigger']		= 'm';
 $config['directory_trigger']	= 'd'; // experimental not currently in use
@@ -180,7 +180,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
--- backend-code/application/controllers/admin.php
+++ backend-code/application/controllers/admin.php
@@ -12,7 +12,7 @@ class Admin extends CI_Controller {
 	}
 
 	public function view($page = 'index') {
-
+		
 		if (!file_exists(APPPATH . '/views/admin/'.$page.'.php')) {
 			show_404();
 		}
